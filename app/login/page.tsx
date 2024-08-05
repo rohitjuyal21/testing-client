712f433d4f4dfbf497af6d68b3a5393742b7e586
Rohit Juyal <rohitjuyal2003@gmail.com>
2024-08-06 00:51:21 +0530

    first commit

---
+++ app/login/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const Page = () => {
+  const [user, setUser] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      console.log(user);
+      router.push("/");
+    }
+  }, [user, router]);
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/user/profile`,
+          { withCredentials: true } // Ensure cookies are sent with the request
+        );
+        setUser(response.data);
+      } catch (err) {
+        setError("Failed to fetch user profile");
+        console.error(err);
+      }
+    };
+
+    fetchUserProfile();
+  }, []);
+
+  const handleLogin = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/google`;
+  };
+
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <button
+        onClick={handleLogin}
+        className="text-2xl border-gray-100 border rounded-md px-6 py-2 w-52"
+      >
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default Page;
