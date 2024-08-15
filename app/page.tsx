ecab948c36adc109457797ea8f1cae51573f34d0
Rohit Juyal <rohitjuyal2003@gmail.com>
2024-08-15 11:01:11 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -34,9 +34,13 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-2">
-      <img src={user.profileImage} alt="" className="rounded-full" />
-      <p>{user.name}</p>
-      <p>{user.email}</p>
+      {user && (
+        <>
+          <img src={user.profileImage} alt="" className="rounded-full" />
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </>
+      )}
     </main>
   );
 }
