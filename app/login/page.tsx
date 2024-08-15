6514a8fc7f9d51b30a8c96731bbb7643b6f5505a
Rohit Juyal <rohitjuyal2003@gmail.com>
2024-08-15 10:54:02 +0530

    updated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,36 +4,8 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const Page = () => {
-  const [user, setUser] = useState<any>(null);
-  const [error, setError] = useState<string | null>(null);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user) {
-      console.log(user);
-      router.push("/");
-    }
-  }, [user, router]);
-
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      try {
-        const response = await axios.get(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/profile`,
-          { withCredentials: true } // Ensure cookies are sent with the request
-        );
-        setUser(response.data);
-      } catch (err) {
-        setError("Failed to fetch user profile");
-        console.error(err);
-      }
-    };
-
-    fetchUserProfile();
-  }, []);
-
   const handleLogin = () => {
-    (window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/google`),
+    (window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/api/login`),
       "_self";
   };
 
